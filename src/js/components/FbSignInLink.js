b43ce5f869ca10e4ee832cd47fb899ea3a7a4b74
Daisuke Taniwaki <daisuketaniwaki@gmail.com>
2017-03-06 15:51:40 +0900

    Use correct permission scope

--- src/js/components/FbSignInLink.js
+++ src/js/components/FbSignInLink.js
@@ -3,7 +3,7 @@ import fb from '../utils/fb';
 import env from '../env';
 
 const FbSignInLink = (_props) => {
-  const url = fb.getLogInUrl(env.FB_APP_ID, ['email', 'ads_management']);
+  const url = fb.getLogInUrl(env.FB_APP_ID, ['email', 'ads_read']);
   return (<button className="btn btn-primary" onClick={(_e) => { window.top.location = url; }}>Sign In</button>);
 };
 
